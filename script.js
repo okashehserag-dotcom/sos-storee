e9b82c8e086a05d374f80304879db4d3c500473b
okashehserag-dotcom <okashehserag@gmail.com>
2026-01-21 16:02:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
 
+
+const images = [
+  "images/soon.png",
+  "images/soon.png",
+  "images/soon.png"
+];
+
 let current = 0;
-let likes = JSON.parse(localStorage.getItem("likes")) || Array(10).fill(0);
-let comments = JSON.parse(localStorage.getItem("comments")) || Array(10).fill([]);
+const slide = document.getElementById("slide");
 
-function openProduct(index) {
-  current = index;
-  document.getElementById("likes").innerText = likes[index];
-  renderComments();
-  document.getElementById("modal").style.display = "flex";
+function next() {
+  current = (current + 1) % images.length;
+  slide.src = images[current];
 }
 
-function closeProduct() {
-  document.getElementById("modal").style.display = "none";
-}
-
-function like() {
-  likes[current]++;
-  localStorage.setItem("likes", JSON.stringify(likes));
-  document.getElementById("likes").innerText = likes[current];
-}
-
-function addComment() {
-  const input = document.getElementById("commentInput");
-  if (!input.value) return;
-
-  comments[current].push(input.value);
-  localStorage.setItem("comments", JSON.stringify(comments));
-  input.value = "";
-  renderComments();
-}
-
-function renderComments() {
-  const box = document.getElementById("comments");
-  box.innerHTML = "";
-  comments[current].forEach(c => {
-    const p = document.createElement("p");
-    p.textContent = c;
-    box.appendChild(p);
-  });
+function prev() {
+  current = (current - 1 + images.length) % images.length;
+  slide.src = images[current];
 }
